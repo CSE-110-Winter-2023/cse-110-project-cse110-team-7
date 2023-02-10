033f5d75ae72730787008cf62ba334dc120280ed
LastEternity <adrian.s.rosing@gmail.com>
2023-02-10 12:31:12 -0800

    Created Social Compass Activity

---
+++ app/src/main/java/com/cse110/team7/socialcompass/ui/CompassActivity.java
@@ -0,0 +1,16 @@
+package com.cse110.team7.socialcompass.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cse110.team7.socialcompass.R;
+
+public class CompassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compass);
+    }
+}
