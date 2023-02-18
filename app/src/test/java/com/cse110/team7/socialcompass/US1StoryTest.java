77f11795d01e30562dc2334245811f2d65c9ef3d
mksudo <104830934+mksudo@users.noreply.github.com>
2023-02-17 22:34:38 -0800

    Revert "Additional unit tests"

--- app/src/test/java/com/cse110/team7/socialcompass/US1StoryTest.java
+++ app/src/test/java/com/cse110/team7/socialcompass/US1StoryTest.java
@@ -1,11 +1,7 @@
 package com.cse110.team7.socialcompass;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import android.graphics.Rect;
-import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 import androidx.lifecycle.Lifecycle;
@@ -18,7 +14,7 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class US1StoryTest   {
     @Test
-    public void US1testCase1() {
+    public void US1test() {
         var scenario = ActivityScenario.launch(CompassActivity.class);
         scenario.moveToState(Lifecycle.State.CREATED);
         scenario.moveToState(Lifecycle.State.STARTED);
@@ -28,28 +24,6 @@ public class US1StoryTest   {
             ImageView north = (ImageView) activity.findViewById(R.id.labelNorth);
             assertNotNull(compass);
             assertNotNull(north);
-        });
-    }
-
-    @Test
-    public void US1testCase2() {
-        var scenario = ActivityScenario.launch(CompassActivity.class);
-        scenario.moveToState(Lifecycle.State.CREATED);
-        scenario.moveToState(Lifecycle.State.STARTED);
-
-        scenario.onActivity(activity -> {
-            ImageView image = (ImageView) activity.findViewById(R.id.labelNorth);
-            Rect rt = image.getDrawable().getBounds();
-
-            int drawLeft = rt.left;
-            int drawTop = rt.top;
-            int drawRight = rt.right;
-            int drawBottom = rt.bottom;
-
-            assertEquals(drawLeft, 0);
-            assertEquals(drawTop, 0);
-            assertEquals(drawRight, 2000);
-            assertEquals(drawBottom, 2000);
         });
     }
 }
