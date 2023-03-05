3bdb352ea281298b348f7836ceeb8f63082bed87
soyoybean <soyonkim00@gmail.com>
2023-03-05 10:10:13 -0800

    calculate distance

---
+++ app/src/main/java/com/cse110/team7/socialcompass/utils/DistanceCalculator.java
@@ -0,0 +1,26 @@
+package com.cse110.team7.socialcompass.utils;
+
+import com.cse110.team7.socialcompass.models.FriendAccount;
+import com.cse110.team7.socialcompass.models.LatLong;
+
+public class DistanceCalculator {
+
+   public static float calculateDistance(LatLong currentLocation, LatLong friendLocation) {
+      final int R = 6371; // Radius of the earth
+
+      double dLa = Math.toRadians(friendLocation.getLatitude() - currentLocation.getLatitude());
+      double dLo = Math.toRadians(friendLocation.getLongitude() - currentLocation.getLongitude());
+      double a = Math.sin(dLa / 2) * Math.sin(dLa / 2)
+              + Math.cos(Math.toRadians(friendLocation.getLatitude())) * Math.cos(Math.toRadians(currentLocation.getLatitude()))
+              * Math.sin(dLo / 2) * Math.sin(dLo / 2);
+      double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      double distance = R * c * 1000; // convert to meters
+
+      return (float)distance;
+   }
+
+   public static float calculateAngle(LatLong currentLocation, FriendAccount friendAccount) {
+      return calculateDistance(currentLocation, friendAccount.getLocation());
+   }
+
+}
