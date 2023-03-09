88a7f3eca9d3bed556d40f1f663ca281b0d2eb9e
mksudo <mkzhanghl@gmail.com>
2023-03-09 00:46:05 -0800

    impl LabeledLocationDao (#131)

---
+++ app/src/main/java/com/cse110/team7/socialcompass/database/LabeledLocationDao.java
@@ -0,0 +1,30 @@
+package com.cse110.team7.socialcompass.database;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Query;
+import androidx.room.Upsert;
+
+import com.cse110.team7.socialcompass.models.LabeledLocation;
+
+import java.util.List;
+
+/**
+ * Data access object for LabeledLocation
+ */
+public abstract class LabeledLocationDao {
+    @Upsert
+    public abstract long upsertLabeledLocation(LabeledLocation labeledLocation);
+
+    @Query("SELECT EXISTS(SELECT 1 FROM `labeled_locations` WHERE publicCode = :publicCode)")
+    public abstract boolean isLabeledLocationExists(String publicCode);
+
+    @Query("SELECT * FROM `labeled_locations` WHERE publicCode = :publicCode")
+    public abstract LiveData<LabeledLocation> selectLabeledLocation(String publicCode);
+
+    @Query("SELECT * FROM `labeled_locations` ORDER BY createdAt")
+    public abstract LiveData<List<LabeledLocation>> selectLabeledLocations();
+
+    @Delete
+    public abstract int deleteLabeledLocation(LabeledLocation labeledLocation);
+}
